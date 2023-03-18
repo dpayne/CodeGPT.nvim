423746e3ed852c99ee0c9aa1e6d617aef1a582a3
Rob Wilson <roobert@gmail.com>
2023-03-18 10:16:09 +0000

    Attempt at loading status..

--- lua/codegpt.lua
+++ lua/codegpt.lua
@@ -3,35 +3,45 @@ local CommandsList = require("codegpt.commands_list")
 local Utils = require("codegpt.utils")
 local CodeGptModule = {}
 
+CodeGptModule.loading_state = {}
+
+function CodeGptModule.status()
+	if next(CodeGptModule.loading_state) == nil then
+		return "CodeGpt: Ready  "
+	else
+		return "CodeGpt: Loading"
+	end
+end
+
 function CodeGptModule.run_cmd(opts)
-  local text_selection = Utils.get_selected_lines()
-  local command_args = table.concat(opts.fargs, " ")
+	local text_selection = Utils.get_selected_lines()
+	local command_args = table.concat(opts.fargs, " ")
 
-  local command = ""
+	local command = ""
 
-  if text_selection ~= "" and command_args ~= "" then
-    if 1 == #opts.fargs and CommandsList.get_cmd_opts(opts.fargs[1]) ~= nil then
-      command = opts.fargs[1]
-      command_args = ""
-    else
-      command = "code_edit"
-    end
-  elseif text_selection ~= "" and command_args == "" then
-    command = "completion"
-  elseif text_selection == "" and command_args ~= "" then
-    command = "chat"
-  end
+	if text_selection ~= "" and command_args ~= "" then
+		if 1 == #opts.fargs and CommandsList.get_cmd_opts(opts.fargs[1]) ~= nil then
+			command = opts.fargs[1]
+			command_args = ""
+		else
+			command = "code_edit"
+		end
+	elseif text_selection ~= "" and command_args == "" then
+		command = "completion"
+	elseif text_selection == "" and command_args ~= "" then
+		command = "chat"
+	end
 
-  if command == nil or command == "" then
-    error("Command not found")
-  end
+	if command == nil or command == "" then
+		error("Command not found")
+	end
 
-  local cmd_opts = CommandsList.get_cmd_opts(command)
-  if cmd_opts == nil then
-    error("Command not found")
-  end
+	local cmd_opts = CommandsList.get_cmd_opts(command)
+	if cmd_opts == nil then
+		error("Command not found")
+	end
 
-  Commands.run_cmd(command, command_args, text_selection)
+	Commands.run_cmd(command, command_args, text_selection)
 end
 
 return CodeGptModule
