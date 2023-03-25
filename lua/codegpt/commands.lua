4d2c8ac72f76de73f23b779dc323e824516d11be
Federico Simonetta <fsimonetta@iccmu.es>
2023-03-25 20:17:36 +0100

    Removed space-only changes

--- lua/codegpt/commands.lua
+++ lua/codegpt/commands.lua
@@ -5,15 +5,15 @@ local Providers = require("codegpt.providers")
 local Commands = {}
 
 function Commands.run_cmd(command, command_args, text_selection)
-  local cmd_opts = CommandsList.get_cmd_opts(command)
-  if cmd_opts == nil then
-    vim.notify("Command not found: " .. command, vim.log.levels.ERROR, {
-      title = "CodeGPT",
-    })
-    return
-  end
+	local cmd_opts = CommandsList.get_cmd_opts(command)
+	if cmd_opts == nil then
+		vim.notify("Command not found: " .. command, vim.log.levels.ERROR, {
+			title = "CodeGPT",
+		})
+		return
+	end
 
-  local request = Providers.get_provider().make_request(command, cmd_opts, command_args, text_selection)
+	local request = Providers.get_provider().make_request(command, cmd_opts, command_args, text_selection)
 
   local bufnr = vim.api.nvim_get_current_buf()
   local start_row, start_col, end_row, end_col = Utils.get_visual_selection()
@@ -24,7 +24,7 @@ function Commands.run_cmd(command, command_args, text_selection)
 end
 
 function Commands.get_status(...)
-  return OpenAiApi.get_status(...)
+	return OpenAiApi.get_status(...)
 end
 
 return Commands
