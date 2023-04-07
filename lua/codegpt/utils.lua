699d44d60129969369a7789e3e29269b104adb63
dpayne <darby.payne@gmail.com>
2023-04-07 14:25:43 -0700

    Never triming code popup text blocks

--- lua/codegpt/utils.lua
+++ lua/codegpt/utils.lua
@@ -1,110 +1,112 @@
 Utils = {}
 
 function Utils.get_filetype()
-	local bufnr = vim.api.nvim_get_current_buf()
-	return vim.api.nvim_buf_get_option(bufnr, "filetype")
+    local bufnr = vim.api.nvim_get_current_buf()
+    return vim.api.nvim_buf_get_option(bufnr, "filetype")
 end
 
 function Utils.get_visual_selection()
-	local bufnr = vim.api.nvim_get_current_buf()
+    local bufnr = vim.api.nvim_get_current_buf()
 
-	local start_pos = vim.api.nvim_buf_get_mark(bufnr, "<")
-	local end_pos = vim.api.nvim_buf_get_mark(bufnr, ">")
+    local start_pos = vim.api.nvim_buf_get_mark(bufnr, "<")
+    local end_pos = vim.api.nvim_buf_get_mark(bufnr, ">")
 
-	if start_pos[1] == end_pos[1] and start_pos[2] == end_pos[2] then
-		return 0, 0, 0, 0
-	end
+    if start_pos[1] == end_pos[1] and start_pos[2] == end_pos[2] then
+        return 0, 0, 0, 0
+    end
 
-	local start_row = start_pos[1] - 1
-	local start_col = start_pos[2]
+    local start_row = start_pos[1] - 1
+    local start_col = start_pos[2]
 
-	local end_row = end_pos[1] - 1
-	local end_col = end_pos[2] + 1
+    local end_row = end_pos[1] - 1
+    local end_col = end_pos[2] + 1
 
-	if vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, true)[1] == nil then
-		return 0, 0, 0, 0
-	end
+    if vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, true)[1] == nil then
+        return 0, 0, 0, 0
+    end
 
-	local start_line_length = vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, true)[1]:len()
-	start_col = math.min(start_col, start_line_length)
+    local start_line_length = vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, true)[1]:len()
+    start_col = math.min(start_col, start_line_length)
 
-	local end_line_length = vim.api.nvim_buf_get_lines(bufnr, end_row, end_row + 1, true)[1]:len()
-	end_col = math.min(end_col, end_line_length)
+    local end_line_length = vim.api.nvim_buf_get_lines(bufnr, end_row, end_row + 1, true)[1]:len()
+    end_col = math.min(end_col, end_line_length)
 
-	return start_row, start_col, end_row, end_col
+    return start_row, start_col, end_row, end_col
 end
 
 function Utils.get_selected_lines()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local start_row, start_col, end_row, end_col = Utils.get_visual_selection()
-	local lines = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
-	return table.concat(lines, "\n")
+    local bufnr = vim.api.nvim_get_current_buf()
+    local start_row, start_col, end_row, end_col = Utils.get_visual_selection()
+    local lines = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
+    return table.concat(lines, "\n")
 end
 
 function Utils.insert_lines(lines)
-	local bufnr = vim.api.nvim_get_current_buf()
-	local line = vim.api.nvim_win_get_cursor(0)[1]
-	vim.api.nvim_buf_set_lines(bufnr, line, line, false, lines)
-	vim.api.nvim_win_set_cursor(0, { line + #lines, 0 })
+    local bufnr = vim.api.nvim_get_current_buf()
+    local line = vim.api.nvim_win_get_cursor(0)[1]
+    vim.api.nvim_buf_set_lines(bufnr, line, line, false, lines)
+    vim.api.nvim_win_set_cursor(0, { line + #lines, 0 })
 end
 
 function Utils.replace_lines(lines, bufnr, start_row, start_col, end_row, end_col)
-	vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, lines)
+    vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, lines)
 end
 
 local function get_code_block(lines2)
-	local code_block = {}
-	local in_code_block = false
-	for _, line in ipairs(lines2) do
-		if line:match("^```") then
-			in_code_block = not in_code_block
-		elseif in_code_block then
-			table.insert(code_block, line)
-		end
-	end
-	return code_block
+    local code_block = {}
+    local in_code_block = false
+    for _, line in ipairs(lines2) do
+        if line:match("^```") then
+            in_code_block = not in_code_block
+        elseif in_code_block then
+            table.insert(code_block, line)
+        end
+    end
+    return code_block
 end
 
 local function contains_code_block(lines2)
-	for _, line in ipairs(lines2) do
-		if line:match("^```") then
-			return true
-		end
-	end
-	return false
+    for _, line in ipairs(lines2) do
+        if line:match("^```") then
+            return true
+        end
+    end
+    return false
+end
+
+function Utils.trim_to_code_block(lines)
+    if contains_code_block(lines) then
+        return get_code_block(lines)
+    end
+    return lines
 end
 
 function Utils.parse_lines(response_text)
-	if vim.g["codegpt_write_response_to_err_log"] then
-		vim.api.nvim_err_write("ChatGPT response: \n" .. response_text .. "\n")
-	end
+    if vim.g["codegpt_write_response_to_err_log"] then
+        vim.api.nvim_err_write("ChatGPT response: \n" .. response_text .. "\n")
+    end
 
-	local lines = vim.fn.split(response_text, "\n")
-	if contains_code_block(lines) then
-		return get_code_block(lines)
-	end
-
-	return lines
+    return vim.fn.split(response_text, "\n")
 end
 
 function Utils.fix_indentation(bufnr, start_row, end_row, new_lines)
-  local original_lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row, true)
-  local min_indentation = math.huge
-  local original_identation = ""
+    local original_lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row, true)
+    local min_indentation = math.huge
+    local original_identation = ""
 
-  -- Find the minimum indentation of any line in original_lines
-  for _, line in ipairs(original_lines) do
-    local indentation = string.match(line, "^%s*")
-    if #indentation < min_indentation then
-      min_indentation = #indentation
-      original_identation = indentation
+    -- Find the minimum indentation of any line in original_lines
+    for _, line in ipairs(original_lines) do
+        local indentation = string.match(line, "^%s*")
+        if #indentation < min_indentation then
+            min_indentation = #indentation
+            original_identation = indentation
+        end
     end
-  end
 
-  -- Change the existing lines in new_lines by adding the old identation
-  for i, line in ipairs(new_lines) do
-    new_lines[i] = original_identation .. line
-  end
+    -- Change the existing lines in new_lines by adding the old identation
+    for i, line in ipairs(new_lines) do
+        new_lines[i] = original_identation .. line
+    end
 end
 
 return Utils
