30ece5a970551a9a810566e28fd3473b3e4a0cbb
duy <nmnduy@gmail.com>
2024-03-14 02:46:17 -0500

    add anthropic provider

--- lua/codegpt/providers/anthropic.lua
+++ lua/codegpt/providers/anthropic.lua
@@ -3,18 +3,12 @@ local Render = require("codegpt.template_render")
 local Utils = require("codegpt.utils")
 local Api = require("codegpt.api")
 
-OpenAIProvider = {}
+AnthropicProvider = {}
 
 local function generate_messages(command, cmd_opts, command_args, text_selection)
-    local system_message = Render.render(command, cmd_opts.system_message_template, command_args, text_selection,
-        cmd_opts)
     local user_message = Render.render(command, cmd_opts.user_message_template, command_args, text_selection, cmd_opts)
 
     local messages = {}
-    if system_message ~= nil and system_message ~= "" then
-        table.insert(messages, { role = "system", content = system_message })
-    end
-
     if user_message ~= nil and user_message ~= "" then
         table.insert(messages, { role = "user", content = user_message })
     end
@@ -39,24 +33,46 @@ local function get_max_tokens(max_tokens, messages)
     return max_tokens - total_length
 end
 
-function OpenAIProvider.make_request(command, cmd_opts, command_args, text_selection)
+function AnthropicProvider.make_request(command, cmd_opts, command_args, text_selection)
+    local system_message = Render.render(command, cmd_opts.system_message_template, command_args, text_selection,
+        cmd_opts)
     local messages = generate_messages(command, cmd_opts, command_args, text_selection)
     local max_tokens = get_max_tokens(cmd_opts.max_tokens, messages)
+    print("max_tokens: " .. max_tokens)
 
     local request = {
-        temperature = cmd_opts.temperature,
-        n = cmd_opts.number_of_choices,
-        model = cmd_opts.model,
-        messages = messages,
+        temperature = cmd_opts.temperature or 1.0,
         max_tokens = max_tokens,
+        model = cmd_opts.model,
+        system = system_message,
+        messages = messages,
     }
 
     return request
 end
 
-local function curl_callback(response, cb)
+function AnthropicProvider.make_headers()
+    local api_key = vim.g["codegpt_anthropic_api_key"] or os.getenv("ANTHROPIC_API_KEY")
+
+    if not api_key then
+        error(
+            "Anthropic API Key not found, set in vim with 'codegpt_anthropic_api_key' or as the env variable 'ANTHROPIC_API_KEY'"
+        )
+    end
+
+    return {
+        ["Content-Type"] = "application/json",
+        ["X-API-Key"] = api_key,
+        ["anthropic-version"] = "2023-06-01",
+    }
+end
+
+
+function curl_callback(response, cb)
     local status = response.status
+    print("Status: " .. status)
     local body = response.body
+    print("Body: " .. body)
     if status ~= 200 then
         body = body:gsub("%s+", " ")
         print("Error: " .. status .. " " .. body)
@@ -69,55 +85,51 @@ local function curl_callback(response, cb)
     end
 
     vim.schedule_wrap(function(msg)
+        print("Msg: " .. msg)
         local json = vim.fn.json_decode(msg)
-        OpenAIProvider.handle_response(json, cb)
+        print("Json: " .. vim.inspect(json))
+        AnthropicProvider.handle_response(json, cb)
     end)(body)
 
     Api.run_finished_hook()
 end
 
-function OpenAIProvider.make_headers()
-    local token = vim.g["codegpt_openai_api_key"]
-    if not token then
-        error(
-            "OpenAIApi Key not found, set in vim with 'codegpt_openai_api_key' or as the env variable 'OPENAI_API_KEY'"
-        )
-    end
 
-    return { Content_Type = "application/json", Authorization = "Bearer " .. token }
-end
-
-function OpenAIProvider.handle_response(json, cb)
+function AnthropicProvider.handle_response(json, cb)
     if json == nil then
         print("Response empty")
     elseif json.error then
         print("Error: " .. json.error.message)
-    elseif not json.choices or 0 == #json.choices or not json.choices[1].message then
-        print("Error: " .. vim.fn.json_encode(json))
+    elseif json.stop_reason ~= "end_turn" then
+        print("Response is incomplete. Payload: " .. vim.fn.json_encode(json))
     else
-        local response_text = json.choices[1].message.content
+        if json.content[1] ~= nil then
+            local response_text = json.content[1].text
 
-        if response_text ~= nil then
-            if type(response_text) ~= "string" or response_text == "" then
-                print("Error: No response text " .. type(response_text))
-            else
-                local bufnr = vim.api.nvim_get_current_buf()
-                if vim.g["codegpt_clear_visual_selection"] then
-                    vim.api.nvim_buf_set_mark(bufnr, "<", 0, 0, {})
-                    vim.api.nvim_buf_set_mark(bufnr, ">", 0, 0, {})
+            if response_text ~= nil then
+                if type(response_text) ~= "string" or response_text == "" then
+                    print("Error: No response text " .. type(response_text))
+                else
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    if vim.g["codegpt_clear_visual_selection"] then
+                        vim.api.nvim_buf_set_mark(bufnr, "<", 0, 0, {})
+                        vim.api.nvim_buf_set_mark(bufnr, ">", 0, 0, {})
+                    end
+                    cb(Utils.parse_lines(response_text))
                 end
-                cb(Utils.parse_lines(response_text))
+            else
+                print("Error: No completion")
             end
         else
-            print("Error: No message")
+            print("Error: No completion")
         end
     end
 end
 
-function OpenAIProvider.make_call(payload, cb)
+function AnthropicProvider.make_call(payload, cb)
     local payload_str = vim.fn.json_encode(payload)
-    local url = "https://api.openai.com/v1/chat/completions"
-    local headers = OpenAIProvider.make_headers()
+    local url = "https://api.anthropic.com/v1/messages"
+    local headers = AnthropicProvider.make_headers()
     Api.run_started_hook()
     curl.post(url, {
         body = payload_str,
@@ -126,10 +138,10 @@ function OpenAIProvider.make_call(payload, cb)
             curl_callback(response, cb)
         end,
         on_error = function(err)
-            print('Error:', err.message)
+            print('Curl error:', err.message)
             Api.run_finished_hook()
         end,
     })
 end
 
-return OpenAIProvider
+return AnthropicProvider
