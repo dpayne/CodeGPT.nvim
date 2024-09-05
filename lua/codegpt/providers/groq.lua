0b89613b5ddf279c85d246a045c1d18088b14df1
duy <nmnduy@gmail.com>
2024-09-05 16:42:07 +0700

    groq; fix max token check

--- lua/codegpt/providers/groq.lua
+++ lua/codegpt/providers/groq.lua
@@ -59,19 +59,14 @@ end
 function GroqProvider.make_request(command, cmd_opts, command_args, text_selection)
     local messages = generate_messages(command, cmd_opts, command_args, text_selection)
 
-    local max_tokens = cmd_opts.max_tokens or 4096
-    local max_tokens_include_context = cmd_opts.max_tokens_include_context or true
-    local max_context_length = cmd_opts.max_context_length or 128 * 1024
-    local check_context_length = cmd_opts.check_context_length or false
-
-    if max_tokens_include_context then
+    if cmd_opts.max_tokens_include_context then
         max_tokens = get_max_output_tokens(cmd_opts.max_tokens, messages)
     else
         max_tokens = cmd_opts.max_tokens
     end
 
-    if check_context_length then
-        fail_if_exceed_context_window(max_context_length, messages)
+    if cmd_opts.check_context_length then
+        fail_if_exceed_context_window(cmd_opts.max_context_length, messages)
     end
 
     local request = {
